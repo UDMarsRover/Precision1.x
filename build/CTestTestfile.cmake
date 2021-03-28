5c000524a6d57ea67d39af9209976532c77b5304
udmrt <udmarsrover@udayton.edu>
2021-03-28 19:56:57 +0100

    iniial push

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/engineer/precision1/src
+# Build directory: /home/engineer/precision1/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
