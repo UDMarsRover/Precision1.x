f83225d9df99efabef4b45c7de956954879e092a
gregmolskow <greg.molskow@gmail.com>
2021-03-31 21:15:49 -0400

    driver test boi

--- src/rover_main/scripts/driver.py
+++ src/rover_main/scripts/driver.py
@@ -3,4 +3,27 @@
 import rospy
 from std_msgs.msg import String
 
+rospy.init_node('driver')
+
+
+# this function handles input from the control device
+def takeInCommnad(dataIn):
+    motors = rospy.Publisher('driverToMotors',String, queue_size = 10)
+    rospy.rospy.Rate(10) #Hz
+    commandIn = dataIn.data
+    rospy.loginfo(commandIn)
+    pub.publish(commandIn)
+    rate.sleep() #Not sure what this does, find out..
+    
+def takeInMotorData(dataIn)
+    rospy.loginfo(dataIn.data) #for now, just log Arduino responce
+
+
+
+if __name__ == '__main__':
+    rospy.Subscriber('controlToDriver', String, takeInCommnad)
+    rospy.Subscriber('motorsToDriver', String, takeInMotorData)
+    
+    rospy.spin()
+
 
