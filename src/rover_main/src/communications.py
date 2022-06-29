01fd2b594f5d1d6cbb22253677f272ed7dfe4f63
gregmolskow <greg.molskow@gmail.com>
2022-06-28 22:34:20 -0400

    comms update

--- src/rover_main/src/communications.py
+++ src/rover_main/src/communications.py
@@ -5,36 +5,44 @@ from std_msgs.msg import String as rosstring
 
 class Communications:
 
-    def __init__(self,name, pullTopic, pushTopic, serialPort, baudRate=57600):
+    def __init__(self,name,serialPort, baudRate=57600):
         # initalize out internal variables and set up the publisher node and comm port!!
         self.__subscriberName = name
-        self.__baudRate = baudRate
-        self.__pushTopic = pushTopic                                    # Save the topic to subscribe to  for debugging
-        self.__pullTopic = pullTopic
-        self.port = serial.Serial(serialPort,self.__baudRate)           # Open the serial com port
-        self.__pub = rospy.Publisher(self.__pullTopic, rosstring, queue_size=10)
-        rospy.init_node(name)
-    
-    # Private function used to take in published data and write it over the comms
-    def __callback(self, dataIn):
-        message = dataIn.data                           # Take in published data from the topic
-        rospy.loginfo(self.__subscriberName + message)  # Create an entry in the log for debugging
-        self.port.write(bytes(message,'utf-8'))               # Write the data to the comms device
-        self.port.write(bytes('\n','utf-8'))                  # Write our termination value to indicate the message is over
-        print("Send Stuff Yo")
-         
+        self.__baudRate     = baudRate
+        self.port           = serial.Serial(serialPort,self.__baudRate)           # Open the serial com port
+        self.error          = None
+        self.DataOutBuffer  = ""
+        self.DataInBuffer   = ""
     # Getter function for getting baud rate
     def getBaud(self):
         return self.__baudRate
     
     # Send data out
-    def pushCheck(self):
-        rospy.Subscriber(self.__pushTopic, rosstring, self.__callback)    # Listen to Topic
+    def writeData(self):
+        try:
+            for i in range(0,len(self.DataOutBuffer)):
+                self.port.write(bytes(self.DataOutBuffer[i],'utf-8'))         # Write the data to the comms device
+            
+            self.port.write(bytes('\n','utf-8'))            # Write our termination value to indicate the message is over
+        except Exception as e:
+            self.error = e
+        
 
     # Check to see if there is data that was sent to the reciver
-    def pullCheck(self):
-        # Com port input here!!!!!!!!!!!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-        rospy.loginfo("Test Output Log")
-        self.__pub.publish(ser_bytes)
+    def readData(self):
+        
+        # Will freeze if no data is passed
+
+        try:
+            ser_bytes = self.port.read_until()
+            ser_bytes = ser_bytes[:len(ser_bytes)-1]
+            self.DataInBuffer = ser_bytes.decode()
+        
+        except Exception as e:
+            self.error = e
+            
+
+        return self.DataInBuffer
+     
 
 
--- src/rover_main/src/main.py
+++ src/rover_main/src/main.py
@@ -1,19 +1,41 @@
 #!/usr/bin/env python3
 import time
 import rospy
+import sys
 from std_msgs.msg import String as rosstring
-
-
+from communications import Communications
 
 pushTopic = 'RoverToBase'
 count = 0
-pub = rospy.Publisher(pushTopic, rosstring, queue_size=10)
+pub = rospy.Publisher(pushTopic, rosstring, queue_size=1)
 rospy.init_node('RoverMain')
+
+comPort = "/dev/ttyUSB0"
+comms = Communications('RoverComs', serialPort = comPort)
+rate = rospy.Rate(10)               # Set the refresh rate
 
 
 if __name__ == '__main__':
-    while True:
-        pub.publish('Go step on a Lego!')
-        time.sleep(2)
-        count += 1
-        print(count)
+    while not rospy.is_shutdown():
+        #Check the comms topic for any ingoing or outgoing message.
+        
+        print("Data")
+        dataIn = comms.readData()
+        print(dataIn)
+        if comms.error != None :
+            print("execption founds")
+            print(comms.error)
+            sys.exit()
+
+        comms.DataOutBuffer = comms.DataInBuffer
+
+        comms.writeData()
+        if comms.error != None :
+            print("execption found")
+            print(comms.error)
+            sys.exit()
+    
+
+
+
+
