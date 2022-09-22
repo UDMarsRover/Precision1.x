e71ef4e04b265d8e73b88bb3e7382a00fbf00fe3
gregmolskow <greg.molskow@gmail.com>
2022-09-21 21:19:20 -0400

    Updated Docker Info

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
+# Source the ROS library
 source /opt/ros/melodic/setup.bash
 
-cd /home/udmrt/precision1
+# Get the most recent version of the repo from the set branch
+cd /home/udmrt/
+branch=cat branch.txt
+git clone https://github.com/UDMarsRover/precision1.git
+git checkout $branch
+git pull
 
+cd precision1
+# Build the ROS project
 catkin_make
 
+# Source the newly built project 
 source ./devel/setup.bash
 
-roscore
+exec $@
