394d8293272b344e5a29425f86006b3a7d11963a
Paul <lachcikp1@udayton.edu>
2022-11-17 19:18:49 -0500

    Temp sensor code

--- src/rover_emo/src/temperature_sensor_code/temperature_sensor_code.ino
+++ src/rover_emo/src/temperature_sensor_code/temperature_sensor_code.ino
@@ -7,55 +7,68 @@
  */
 
 #include <Arduino_HTS221.h>
+#define GREEN 23
 
-String list[] = {"FF", "FF", "FF", "FF"};
+//String list[] = {"FF", "FF", "FF", "FF"};
 
-double prevTemp = 0;
-double currTemp = 0;
+//double prevTemp = 0;
+//double currTemp = 0;
+float t = 0;
 
 //String temperatureSensor();
 //void startTemperatureSensor();
 
 void setup() {
   Serial.begin(9600);
-  
-  delay(1000);
+  pinMode(GREEN, OUTPUT);
+
+  //delay(1000);
 
   Serial.println("Starting");
   startTemperatureSensor();
+  delay(2000);
 }
 
 void loop() {
-
+  /*
   Serial.print("Temperature: ");
   String temp = temperatureSensor();
   
   Serial.println(temp);
+  delay(1000);
+*/
+  digitalWrite(GREEN, LOW);
+  t = HTS.readTemperature();
+  Serial.print("temp: ");
+  Serial.println(temperatureSensor());
+  digitalWrite(GREEN, HIGH);
+  //delay(500);
 }
 
-void startTemperatureSensor() {
+bool startTemperatureSensor() {
   // Tests if the temperature sensor is able to be initialized
   if (!HTS.begin()) {
     // Temperature sensor was unable to begin
     Serial.println("Could not initialize temperature sensor.");
-    list[0] = "3B";
+    return false;
+    //list[0] = "3B";
   }
 
+  return true;
 }
+
 
 String temperatureSensor() {
   // Takes the temperature reading, and converts it to a 3 character string without the decimal
 
-  currTemp = HTS.readTemperature();
-   
-  // double tempOut = filterFunction(alpha, currTemp, prevTemp);
-  
-  prevTemp = currTemp;
+  if (!HTS.begin()){
+    float currTemp = HTS.readTemperature();
+    // std::string temp_reading = (std::to_string(tempOut * 10)).substr(0, 3);
+    String temp_reading = (String)(currTemp * 10);
+    //std::string temp_reading = (std::to_string(HTS.readTemperature() * 10)).substr(0, 3);
 
-  // std::string temp_reading = (std::to_string(tempOut * 10)).substr(0, 3);
-  String temp_reading = (String)(currTemp * 10);
-  //std::string temp_reading = (std::to_string(HTS.readTemperature() * 10)).substr(0, 3);
-
-  // Returns the temperature
-  return temp_reading;
+    // Returns the temperature
+    return temp_reading;
+  }
+  return "3B";//could not init
 }
