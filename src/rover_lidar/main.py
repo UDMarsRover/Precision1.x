a8c8aab22d234eee031a0ce2f202d250a86620bc
Ryan Kuederle <ryankuederle@icloud.com>
2023-02-02 22:16:03 -0500

    Fixed laserscan import.

--- src/rover_lidar/main.py
+++ src/rover_lidar/main.py
@@ -11,7 +11,7 @@ class Zones():
             print("BRUH")
             rospy.init_node('deez_nuts')
             arr = Float32MultiArray()
-            print(LaserScan.ranges[0])
+            print(msg.ranges[0])
             # arr.data = [LaserScan.ranges[0], 0, 0, 0, 0, 0, 0, 0]
             # self.pub.publish(arr)
 
