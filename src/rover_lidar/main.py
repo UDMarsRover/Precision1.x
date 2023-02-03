c2191ff5f18a5074604d372399f237df2ac69888
Ryan Kuederle <ryankuederle@icloud.com>
2023-02-02 22:23:02 -0500

    Fixed laserscan import.

--- src/rover_lidar/main.py
+++ src/rover_lidar/main.py
@@ -3,30 +3,37 @@ import rospy
 from sensor_msgs.msg import LaserScan
 from std_msgs.msg import Float32MultiArray
 
-class Zones():
-    def __init__(self):
-        self.pub = rospy.Publisher('LidarToPi', Float32MultiArray, queue_size=10)
+# class Zones():
+#     def __init__(self):
+#         self.pub = rospy.Publisher('LidarToPi', Float32MultiArray, queue_size=10)
         
     
 
-    def stream(self):
-        def callback(msg):
-            print("BRUH")
-            rospy.init_node('deez_nuts')
-            arr = Float32MultiArray()
-            print(msg.ranges[0])
-            # arr.data = [LaserScan.ranges[0], 0, 0, 0, 0, 0, 0, 0]
-            # self.pub.publish(arr)
+#     def stream(self):
+#         def callback(msg):
+#             print("BRUH")
+#             rospy.init_node('deez_nuts')
+#             arr = Float32MultiArray()
+#             print(msg.ranges[0])
+#             # arr.data = [LaserScan.ranges[0], 0, 0, 0, 0, 0, 0, 0]
+#             # self.pub.publish(arr)
 
-        print("Running ros loop...")
-        print("Data recieved from lidar.")
-        self.sub = rospy.Subscriber("scan", LaserScan, callback())
-        rospy.spin()
+#         print("Running ros loop...")
+#         print("Data recieved from lidar.")
+#         self.sub = rospy.Subscriber("scan", LaserScan, callback())
+#         rospy.spin()
 
 
     
 
 
-if __name__ == '__main__':
-    zone = Zones()
-    zone.stream()
+# if __name__ == '__main__':
+#     zone = Zones()
+#     zone.stream()
+
+def callback(msg):
+    print(len(msg.ranges))
+
+rospy.init_node('scan_values')
+sub = rospy.Subscriber('/kobuki/laser/scan', LaserScan, callback)
+rospy.spin()
