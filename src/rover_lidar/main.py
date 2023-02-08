628e50cc61d04629af451dbb29f218072f3859d0
Ryan Kuederle <ryankuederle@icloud.com>
2023-02-07 20:28:40 -0500

    Bug Fixes

--- src/rover_lidar/main.py
+++ src/rover_lidar/main.py
@@ -7,6 +7,7 @@ pub = rospy.Publisher('LidarToPi', Float32MultiArray, queue_size=10)
 def callback(msg):
     arr = Float32MultiArray()
     arr.data = [msg.ranges[0]]
+    print("Number of values in ranges" + str(len(msg.ranges)))
     print("Minimum angle: " + str(msg.angle_min))
     print("Maximum angle: " + str(msg.angle_max))
     print("Angle increment" + str(msg.angle_increment))
