5ad9dabd5bffefe4f66c2456459a91ee725b5470
Ryan Kuederle <ryankuederle@icloud.com>
2023-02-07 20:16:24 -0500

    Added print statement for max and min values

--- src/rover_lidar/main.py
+++ src/rover_lidar/main.py
@@ -3,38 +3,14 @@ import rospy
 from sensor_msgs.msg import LaserScan
 from std_msgs.msg import Float32MultiArray
 
-# class Zones():
-#     def __init__(self):
-#         self.pub = rospy.Publisher('LidarToPi', Float32MultiArray, queue_size=10)
-        
-    
-
-#     def stream(self):
-#         def callback(msg):
-#             print("BRUH")
-#             rospy.init_node('deez_nuts')
-#             arr = Float32MultiArray()
-#             print(msg.ranges[0])
-#             # arr.data = [LaserScan.ranges[0], 0, 0, 0, 0, 0, 0, 0]
-#             # self.pub.publish(arr)
-
-#         print("Running ros loop...")
-#         print("Data recieved from lidar.")
-#         self.sub = rospy.Subscriber("scan", LaserScan, callback())
-#         rospy.spin()
-
-
-    
-
-
-# if __name__ == '__main__':
-#     zone = Zones()
-#     zone.stream()
 pub = rospy.Publisher('LidarToPi', Float32MultiArray, queue_size=10)
 def callback(msg):
     arr = Float32MultiArray()
     arr.data = [msg.ranges[0]]
+    print("Minimum angle: " + msg.range_min)
+    print("Maximum angle: " + msg.range_max)
     pub.publish(arr)
+
 
 
 rospy.init_node('scan_values')
