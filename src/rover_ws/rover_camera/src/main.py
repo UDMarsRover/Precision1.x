7975319b2f0bf6198b03097250931c6cad672f5a
Kaiden Keiser <kaidenpkeiser@gmail.com>
2023-02-16 21:21:42 -0500

    image compression v1

--- src/rover_ws/rover_camera/src/main.py
+++ src/rover_ws/rover_camera/src/main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import PIL
 import cv2
 import rospy
 from sensor_msgs.msg import Image
@@ -7,6 +8,7 @@ from cv_bridge import CvBridge
 import numpy as np
 
 class Camera:
+    
     def __init__(self, camFile = -1, fps=20, width=320, height=240):
         # Setup camera
         self.fps = fps
@@ -17,6 +19,23 @@ class Camera:
         self.fps = fps
         rospy.init_node('camera_node')
 
+    def compressImage(self, image, factor = 2):
+        width, height = image.size
+        newImage = PIL.Image.new("RGB", (width / factor, height / factor), color = 0)
+        newWidth, newHeight = newImage.size
+        pixelMap = newImage.load
+        
+        for i in range(newWidth):
+            for j in range (newHeight):
+                r, g, b = 0
+                r, g, b += image.getPixel(i * 2, j * 2)
+                r, g, b += image.getPixel(i * 2 + 1, j * 2)
+                r, g, b += image.getPixel(i * 2, j * 2 + 1)
+                r, g, b += image.getPixel(i * 2 + 1, j * 2 + 1)
+                r, g, b /= 4
+                pixelMap[i,j] = (r, g, b)
+        return newImage
+    
     def run(self): 
         rate = rospy.Rate(self.fps)
         bridge = CvBridge()
