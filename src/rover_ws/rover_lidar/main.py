bfb256a96ed52645667d6d27a723239ef93d4f82
Ryan Kuederle <ryankuederle@icloud.com>
2023-03-02 21:57:10 -0500

    Fixed some lidar stuff

--- src/rover_ws/rover_lidar/main.py
+++ src/rover_ws/rover_lidar/main.py
@@ -14,7 +14,8 @@ def callback(msg):
     # 3  4  5
     def getZone(i):
         lenr = len(msg.ranges)
-        startidx = int(lenr / 16)        space = lenr / 8
+        startidx = int(lenr / 16)        
+        space = lenr / 8
         starts = [0, startidx, startidx + space * 1, startidx + space * 2, startidx + space * 3,
              startidx + space * 4, startidx + space * 5, startidx + space * 6, startidx + space * 7]
         if i == 7:
