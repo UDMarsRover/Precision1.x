211ae38deec03e57a4633c16bfc704510ad2abee
Ryan Kuederle <ryankuederle@icloud.com>
2023-03-02 21:18:57 -0500

    Fixed some lidar stuff

--- src/rover_ws/rover_lidar/main.py
+++ src/rover_ws/rover_lidar/main.py
@@ -23,19 +23,9 @@ def callback(msg):
         else:
             left = msg.ranges[0 : increment]
             right = msg.ranges[len(msg.ranges) - increment : len(msg.ranges) - 1]
-            minval = min(left)
-
-        # increment = int(len(msg.ranges) / 16)
-        # start = int(i * increment)
-        # stop = int((i + 2) * increment)
-        # arr = msg.ranges[start : stop]
-        # if (i == 0): 
-        #     start1 = int(len(msg.ranges) - increment)
-        #     end1 = int(len(msg.ranges) - 1)
-        #     start2 = int(0)
-        #     end2 = increment
-        #     arr = numpy.concatenate(msg.ranges[start1 : end1], msg.ranges[start2 : end2])
-        #     arr = msg.ranges[start1 : end1]
+            minleft = min(left)
+            minright = min(right)
+            minval = min(minleft, minright)
 
         
 
