dfca93d9528bee3d68adfadde091f2ff9145c3f6
Ryan Kuederle <ryankuederle@icloud.com>
2023-03-07 20:55:14 -0500

    Modified processing files.

--- src/rover_ws/rover_lidar/publish_processed.py
+++ src/rover_ws/rover_lidar/publish_processed.py
@@ -19,9 +19,44 @@ def callback(msg):
     filtered_scan = gaussian_filter(scan, sigma=0.5)
 
     # Print or plot the filtered scan array
-    print(filtered_scan)
+    print(find_closest_objects(filtered_scan))
 
-
+# Define a function to find the 5 closest objects in the scan array
+def find_closest_objects(scan):
+    # Initialize an empty list to store the objects data
+    objects = []
+    # Loop through each value in the scan array
+    for i in range(len(scan)):
+        # Get the distance and angle of the current value
+        distance = scan[i]
+        angle = i * np.pi / 380 # Assuming 760 values cover 180 degrees
+        # Check if the current value is smaller than a threshold (e.g. 10)
+        if distance < 10:
+            # Check if there is already an object in the list
+            if objects:
+                # Get the last object in the list
+                last_object = objects[-1]
+                # Check if the current value is close enough to the last object (e.g. within 5 degrees)
+                if abs(angle - last_object["end_angle"]) < np.pi / 36:
+                    # Update the last object's end angle and width
+                    last_object["end_angle"] = angle
+                    last_object["width"] = distance * (last_object["end_angle"] - last_object["start_angle"])
+                else:
+                    # Create a new object with start angle, end angle and width attributes
+                    new_object = {"start_angle": angle, "end_angle": angle, "width": 0}
+                    # Append the new object to the list of objects
+                    objects.append(new_object)
+            else:
+                # Create a new object with start angle, end angle and width attributes
+                new_object = {"start_angle": angle, "end_angle": angle, "width": 0}
+                # Append the new object to the list of objects
+                objects.append(new_object)
+    
+    # Sort the list of objects by their distance (ascending order)
+    objects.sort(key=lambda obj: scan[int(obj["start_angle"] * 380 / np.pi)])
+    
+    # Return only the first 5 objects in the list 
+    return objects[:5]
 
 
 
