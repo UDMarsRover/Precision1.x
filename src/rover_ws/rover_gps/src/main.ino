9b0b63a9849229e9e74d518042616e6c9a8a3b2f
Kaiden K <keiserk4@udayton.edu>
2023-05-02 20:41:53 -0400

    gps main file

--- src/rover_ws/rover_gps/src/main.ino
+++ src/rover_ws/rover_gps/src/main.ino
@@ -1,10 +1,33 @@
 #include <Arduino.h>
 #include <TinyGPSPlus.h>
+#include <SoftwareSerial.h>
+
+
+static const int RXPin = 7, TXPin = 8;
+TinyGPSPlus gps;
+SoftwareSerial ss(RXPin, TXPin);
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  ss.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  while (ss.available() > 0)
+    if (gps.encode(ss.read()))
+      displayInfo();
+
+  if (millis() > 5000 && gps.charsProcessed() < 10)
+  {
+    Serial.println(F("No GPS detected: check wiring."));
+    while(true);
+  }
+}
+
+void displayInfo()
+{
+  Serial.print(gps.location.lat(), 6);
+  Serial.print(", ");
+  Serial.print(gps.location.lng(), 6);
+  Serial.print("\n");
 }
