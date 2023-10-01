8cf5316954a6dfc2d8d69f6d0d52f089e452124f
Ben Russell <pandaben@me.com>
2023-10-01 18:03:30 -0400

    i forgot to stage this one

--- src/rover_pkg/MRCamera/camera_publisher.py
+++ src/rover_pkg/MRCamera/camera_publisher.py
@@ -1,9 +1,26 @@
 #!/usr/bin/env python3
-import cv2 as cv
 import rospy
+import cv2 as cv
+import numpy as np
 from cv_bridge import CvBridge
 from std_msgs.msg import String
 from sensor_msgs.msg import Image
+
+# def compress_image(image, f = 3):
+#     new_width = int(len(image[0]) / f)
+#     new_height = int(len(image) / f)
+#     pixel_map = np.zeros((int(new_height),int(new_width),3), dtype = int)
+        
+#     for i in range(new_height):
+#         newI = i*f
+#         for j in range(new_width):
+#             newJ = j*f
+#             subimage = image[newI:newI+f, newJ:newJ+f, :]
+#             outtemp = np.sum(subimage, axis=(0,1))
+#             p = np.asarray(outtemp/(f*f), dtype= 'int')
+#             pixel_map[i][j] = p
+
+#     return pixel_map
 
 def start_node():
     # create node
@@ -30,16 +47,19 @@ def start_node():
             print("Can't recieve frame")
             break
 
+        #compress frame
+        #compressed_frame = compress_image(frame)
+
         #display frame
-        cv.imshow('video', frame)
+        cv.imshow('video', compressed_frame)
         if cv.waitKey(1) == ord('q'):
             break
 
         # convert from opencv image to ros image
-        imgMsg = bridge.cv2_to_imgmsg(frame, "bgr8")
+        img_msg = bridge.cv2_to_imgmsg(compressed_frame, "bgr8")
 
         # publish to topic
-        pub.publish(imgMsg)
+        pub.publish(img_msg)
         rospy.Rate(24.0).sleep()  # 1 Hz
     
     # release capture and destroy windows
