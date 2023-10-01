b739a126ccd77e28318c5e34c6ece0825bccc136
Ben Russell <pandaben@me.com>
2023-10-01 18:03:02 -0400

    cleaned up commets, tried to implement compression

--- src/rover_pkg/MRCamera/camera_publisher.py
+++ src/rover_pkg/MRCamera/camera_publisher.py
@@ -1,32 +1,16 @@
 #!/usr/bin/env python3
-import sys
 import cv2 as cv
 import rospy
 from cv_bridge import CvBridge
 from std_msgs.msg import String
 from sensor_msgs.msg import Image
 
-# capture = cv.VideoCapture(0)
-# if not capture.isOpened():
-#     print("Cannot open camera")
-#     exit(0)
-
-# while True:
-#     ret, frame = capture.read()
-#     image_message = bridge.cv2_to_imgmsg(frame, encoding="passthrough")
-
-#     if not ret:
-#         print("Can't recieve frame")
-#         break
-
 def start_node():
+    # create node
     rospy.init_node('image_pub')
     rospy.loginfo('image_pub node started')
 
-    # img = cv.imread(filename)
-    # cv.imshow("image", img)
-    # cv.waitKey(2000)
-
+    # create bridge and video capture
     bridge = CvBridge()
     capture = cv.VideoCapture(0) 
 
@@ -34,8 +18,10 @@ def start_node():
         print("cannot open camera")
         exit()
 
+    # create publisher with image data type
     pub = rospy.Publisher('image', Image, queue_size=10)
 
+    # main control loop
     while not rospy.is_shutdown():
         #capture frame by frame
         ret, frame = capture.read()
@@ -43,12 +29,16 @@ def start_node():
         if not ret:
             print("Can't recieve frame")
             break
+
         #display frame
         cv.imshow('video', frame)
         if cv.waitKey(1) == ord('q'):
             break
 
+        # convert from opencv image to ros image
         imgMsg = bridge.cv2_to_imgmsg(frame, "bgr8")
+
+        # publish to topic
         pub.publish(imgMsg)
         rospy.Rate(24.0).sleep()  # 1 Hz
     
@@ -56,10 +46,8 @@ def start_node():
     capture.release()
     cv.destroyAllWindows()
 
-
 if __name__ == '__main__':
     try:
-        # start_node(rospy.myargv(argv=sys.argv)[1])
         start_node()
     except rospy.ROSInterruptException:
         pass
