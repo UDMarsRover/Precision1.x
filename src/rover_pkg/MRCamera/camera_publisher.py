aeece3c12d0a652b2669ebf1a31ffcf410f1d4f5
Ben Russell <pandaben@me.com>
2023-10-08 16:36:43 -0400

    big overhaul of camera pub and sub

--- src/rover_pkg/MRCamera/camera_publisher.py
+++ src/rover_pkg/MRCamera/camera_publisher.py
@@ -1,72 +1,34 @@
 #!/usr/bin/env python3
 import rospy
-import cv2 as cv
-import numpy as np
-from cv_bridge import CvBridge
-from std_msgs.msg import String
 from sensor_msgs.msg import Image
-
-# def compress_image(image, f = 3):
-#     new_width = int(len(image[0]) / f)
-#     new_height = int(len(image) / f)
-#     pixel_map = np.zeros((int(new_height),int(new_width),3), dtype = int)
-        
-    # for i in range(new_height):
-    #     newI = i*f
-    #     for j in range(new_width):
-    #         newJ = j*f
-    #         subimage = image[newI:newI+f, newJ:newJ+f, :]
-    #         outtemp = np.sum(subimage, axis=(0,1))
-    #         p = np.asarray(outtemp/(f*f), dtype= 'int64')
-    #         pixel_map[i][j] = p
-
-    # return pixel_map
+from cv_bridge import CvBridge
+import cv2
 
 def start_node():
-    # create node
-    rospy.init_node('image_pub')
-    rospy.loginfo('image_pub node started')
+    # create node and publisher
+    rospy.init_node('camera_sensor_publisher', anonymous = True)
+    publisher = rospy.Publisher('video_topic', Image, queue_size = 60)
 
-    # create bridge and video capture
+    # establish rate in HZ 
+    rate = rospy.Rate(30)
+
+    # create video capture and bridge
+    video_capture = cv2.VideoCapture(0)
     bridge = CvBridge()
-    capture = cv.VideoCapture(0) 
-
-    if not capture.isOpened():
-        print("cannot open camera")
-        exit()
-
-    # create publisher with image data type
-    pub = rospy.Publisher("camera", Image, queue_size=10)
 
     # main control loop
     while not rospy.is_shutdown():
         #capture frame by frame
-        ret, frame = capture.read()
-        # capture.set(cv.CAP_PROP_FRAME_WIDTH, 320)
-        # capture.set(cv.CAP_PROP_FRAME_HEIGHT, 240)
+        ret, frame = video_capture.read()
 
-        if not ret:
-            print("Can't recieve frame")
-            break
+        if ret == True:
+            rospy.loginfo('Frame captured and published')
 
-        #compress frame
-        #compressed_frame = compress_image(frame)
+            # convert to imgmsg and publish to topic
+            image_to_topic = bridge.cv2_to_imgmsg(frame)
+            publisher.publish(image_to_topic)
 
-        #display frame
-        # cv.imshow('video', frame)
-        # if cv.waitKey(1) == ord('q'):
-        #     break
-
-        # convert from opencv image to ros image
-        img_msg = bridge.cv2_to_imgmsg(frame, "bgr8")
-
-        # publish to topic
-        pub.publish(img_msg)
-        rospy.Rate(24.0).sleep()  # 1 Hz
-    
-    # release capture and destroy windows
-    capture.release()
-    cv.destroyAllWindows()
+        rate.sleep()
 
 if __name__ == '__main__':
     try:
--- src/rover_pkg/MRCamera/camera_subscriber.py
+++ src/rover_pkg/MRCamera/camera_subscriber.py
@@ -1,26 +1,27 @@
 #!/usr/bin/env python3
 import rospy
-import cv2 as cv
-import numpy as np
-from cv_bridge import CvBridge
-from std_msgs.msg import String
 from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+import cv2
 
-def image_callback(img_msg):
-    rospy.loginfo(img_msg)
-    # frame = bridge.imgmsg_to_cv2(img_msg, "bgr8")
-    # cv.imshow("video", frame)
-    # cv.waitKey(1)
-
-if __name__ == '__main__':
-    # create node
-    rospy.init_node("image_sub")
-    rospy.loginfo("image_sub node started")
-
+def callback(msg):
     # create bridge
     bridge = CvBridge()
 
-    #create subscriber with image data type
-    sub = rospy.Subscriber("camera", Image, callback=image_callback)
+    rospy.loginfo("recieved a video message/frame")
 
+    # convert back to cv and display image
+    cv_image = bridge.imgmsg_to_cv2(msg)
+    cv2.imshow("camera", cv_image)
+
+    cv2.waitKey(1)
+
+if __name__ == '__main__':
+    # create node and subscriber
+    rospy.init_node('camera_sensor_subscriber', anonymous=True)
+    rospy.Subscriber('video_topic', Image, callback)
+
+    # keep node running until shut down
     rospy.spin()
+
+    cv2.destroyAllWindows()
