73803cc71683505f23d7fc9ce066dbc1a365fcbb
gregmolskow <greg.molskow@gmail.com>
2023-10-08 19:16:08 -0400

    coms update

---
+++ src/lrc_pkg/main_rover.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+import rospy
+from sensor_msgs.msg import Image
+from std_msgs.msg import String
+from RFD900 import RFD900 as comms
+
+comPort1 = "/dev/ttyUSB1"
+resolution = [720,1280] #hight, width
+#comPort2 = "/dev/ttyUSB1"
+commsOut = comms(comPort1)
+#camCom = comms(comPort1)
+
+# Initialize the Rover ROS_COMMS node
+rospy.init_node('rover_lrc', anonymous = True)
+
+comSub = rospy.Subscriber('RoverToBase', String,  commsOut.writeString)
+comPub = rospy.Publisher('BaseToRover', String, queue_size = 10)
+#camSub = rospy.Subscriber('CameraToBase', Image,  camCom.readImage)
+#camPub = rospy.Publisher('BaseToCamera', Image, queue_size = 10)
+rate = rospy.Rate(10)               # Set the refresh rate
+
+#camCom.openImageStream(resolution)
+
+if (__name__ == '__main__'):
+    # While roscore is up, running and connencted
+    while not rospy.is_shutdown():
+
+        # Get Incoming Data
+
+        data = commsOut.dataInBuffer
+
+        # Write Outgoing Data
+        comPub.publish(data)
+
+        #camCom.readImage()
+
+        rate.sleep()
+
