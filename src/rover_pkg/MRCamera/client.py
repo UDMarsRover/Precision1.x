d20bf29e7dc7818b3d4547660c10fed4e70a3416
Ben Russell <pandaben@me.com>
2023-11-10 16:15:51 -0500

    unsuccessful socket streaming

--- src/rover_pkg/MRCamera/client.py
+++ src/rover_pkg/MRCamera/client.py
@@ -1,28 +1,37 @@
-import socket,cv2, pickle,struct
+#importing libraries
+import socket
+import cv2
+import pickle
+import struct
 
-# create socket
+# Client socket
+# create an INET, STREAMing socket : 
 client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-host_ip = '192.168.1.20' # paste your server ip address here
-port = 1234 
-client_socket.connect((host_ip,port)) # a tuple
+host_ip = '127.0.0.1'# Standard loopback interface address (localhost)
+port = 1234 # Port to listen on (non-privileged ports are > 1023)
+# now connect to the web server on the specified port number
+client_socket.connect((host_ip,port)) 
+#'b' or 'B'produces an instance of the bytes type instead of the str type
+#used in handling binary data from network connections
 data = b""
+# Q: unsigned long long integer(8 bytes)
 payload_size = struct.calcsize("Q")
+
 while True:
-	while len(data) < payload_size:
-		packet = client_socket.recv(4*1024) # 4K
-		if not packet: break
-		data+=packet
-	packed_msg_size = data[:payload_size]
-	data = data[payload_size:]
-	msg_size = struct.unpack("Q",packed_msg_size)[0]
-	
-	while len(data) < msg_size:
-		data += client_socket.recv(4*1024)
-	frame_data = data[:msg_size]
-	data  = data[msg_size:]
-	frame = pickle.loads(frame_data)
-	cv2.imshow("RECEIVING VIDEO",frame)
-	key = cv2.waitKey(1) & 0xFF
-	if key  == ord('q'):
-		break
+    while len(data) < payload_size:
+        packet = client_socket.recv(4*1024)
+        if not packet: break
+        data+=packet
+    packed_msg_size = data[:payload_size]
+    data = data[payload_size:]
+    msg_size = struct.unpack("Q",packed_msg_size)[0]
+    while len(data) < msg_size:
+        data += client_socket.recv(4*1024)
+    frame_data = data[:msg_size]
+    data  = data[msg_size:]
+    frame = pickle.loads(frame_data)
+    cv2.imshow("Receiving...",frame)
+    key = cv2.waitKey(10) 
+    if key  == 13:
+        break
 client_socket.close()
--- src/rover_pkg/MRCamera/server.py
+++ src/rover_pkg/MRCamera/server.py
@@ -1,40 +1,42 @@
+#importing libraries
 import socket
 import cv2
 import pickle
 import struct
-import imutils
 
-# Socket Create
+# Server socket
+# create an INET, STREAMing socket
 server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 host_name  = socket.gethostname()
 host_ip = socket.gethostbyname(host_name)
 print('HOST IP:',host_ip)
-port =1234 
+port = 1234
 socket_address = (host_ip,port)
-
-# Socket Bind
+print('Socket created')
+# bind the socket to the host. 
+#The values passed to bind() depend on the address family of the socket
 server_socket.bind(socket_address)
-
-# Socket Listen
+print('Socket bind complete')
+#listen() enables a server to accept() connections
+#listen() has a backlog parameter. 
+#It specifies the number of unaccepted connections that the system will allow before refusing new connections.
 server_socket.listen(5)
-print("LISTENING AT:",socket_address)
+print('Socket now listening')
 
-# Socket Accept
+
+
 while True:
-	client_socket,addr = server_socket.accept()
-	print('GOT CONNECTION FROM:',addr)
-	if client_socket:
-		vid = cv2.VideoCapture(0)
-		
-		while(vid.isOpened()):
-			img,frame = vid.read()
-			frame = imutils.resize(frame,width=320)
-			a = pickle.dumps(frame)
-			message = struct.pack("Q",len(a))+a
-			client_socket.sendall(message)
-			
-			cv2.imshow('TRANSMITTING VIDEO',frame)
-			key = cv2.waitKey(1) & 0xFF
-			if key ==ord('q'):
-				client_socket.close()
-				
+    client_socket,addr = server_socket.accept()
+    print('Connection from:',addr)
+    if client_socket:
+        vid = cv2.VideoCapture(0)
+        while(vid.isOpened()):
+            img,frame = vid.read()
+            a = pickle.dumps(frame)
+            message = struct.pack("Q",len(a))+a
+            client_socket.sendall(message)
+            cv2.imshow('Sending...',frame)
+            key = cv2.waitKey(10) 
+            if key ==13:
+                client_socket.close()
+
