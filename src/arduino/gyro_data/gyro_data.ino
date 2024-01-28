cb3034ee84411bf865fc288e28c6e74439259b78
Paul <lachcikp1@udayton.edu>
2024-01-28 16:41:29 -0500

    first attempt at calculus

--- src/arduino/gyro_data/gyro_data.ino
+++ src/arduino/gyro_data/gyro_data.ino
@@ -1,20 +1,13 @@
 
 #include <Arduino_LSM9DS1.h>
 
-//The gyroscope and accelerometer methods are switched in this library for some reason. The readGyroscope() method provides speed of angle change in degrees per second and the 
-//readAccelerometer() method provides positional information for X, Y, and Z on a scale of -1 to 1. Therefore, we do not need to use readGyroscope(), even though these are the values
-//we are looking for.
-float acelX, acelY, acelZ; 
-String errorCodes;
-String gyroError;
-/*
-This is an array of error codes. They are inserted into the array as they come in, and we will pull from the start of the array. Once we pull a value, all following values will be shifted up
-so that 
-*/
+float gyroX, gyroY, gyroZ;
+float curGyroX, curGyroY, curGyroZ = 0;
+float curRoll, curPitch, curYaw; 
+float roll, pitch, yaw;
 
 
 void setup() {
-  // put your setup code here, to run once: 
   Serial.begin(9600);
   while (!Serial);
   Serial.println("Begun");
@@ -33,50 +26,29 @@ void loop() {
 
 void gyroscope() { //returns 3 hex digits
   if (IMU.gyroscopeAvailable()) {
-    //IMU.readGyroscope(gyroX, gyroY, gyroZ); //Provides speed of angle change in degrees per second
-    IMU.readAcceleration(acelX, acelY, acelZ); //Provides positional information for X, Y, and Z on a scale of -1 to 1
+    IMU.readGyroscope(gyroX, gyroY, gyroZ); //Provides angular velocity in degrees/sec
   }
-  /*
-  if(gyroY < minusThreshold || plusThreshold < gyroY)
-  {
-    gyroError = "2F";
-    Serial.println(gyroError);
-    delay(500);
-  }
-  if(gyroX < minusThreshold || plusThreshold < gyroX)
-  {
-    gyroError = "2F";
-    Serial.println(gyroError);
-    delay(500);
-  }
+  
+  Serial.print(gyroY);
+  Serial.print(", ");
+  Serial.print(gyroX);
+  Serial.print(", ");
+  Serial.print(curGyroX);
 
-  if(gyroY < minusThreshold*2 || plusThreshold*2 < gyroY)
-  {
-    gyroError = "30";
-    Serial.println(gyroError);
-    delay(500);
+  bool negative = gyroX < 0;
+  float tan = (gyroX - curGyroX);
+  float integrand = 0.001 * ( pow( (tan), 2 ) / 2 );
+  if (negative) { 
+    pitch = pitch - integrand;
   }
-  if(gyroX < minusThreshold*2 || plusThreshold*2 < gyroX)
-  {
-    gyroError = "30";
-    Serial.println(gyroError);
-    delay(500);
+  else {
+    pitch = pitch + integrand;
   }
-  */
-  Serial.print(acelX);
-  Serial.print(",");
-  Serial.println(acelY);
-  String X = (String)map(acelX*100, -100, 100, 0, 360);
-  String Y = (String)map(acelY*100, -100, 100, 0, 360);
-  Serial.print(X);
-  Serial.print(",");
-  Serial.println(Y);
-  /*
-  String gyro_reading_X = (String)(acelX);
-  String gyro_reading_Y = (String)(acelY);
+  curGyroX = gyroX;
 
-  Serial.println(gyro_reading_X.substring(0,4) + "," + gyro_reading_Y.substring(0,4));
-  */
-  delay(500);
+  Serial.print(", ");
+  Serial.println(pitch);
+
+  delay(1);
 
 }
