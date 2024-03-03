90aece0d4bfe5e8d52eb07a6f09dcc520f4395d5
Paul <lachcikp1@udayton.edu>
2024-03-03 18:30:18 -0500

    Ultrasonic FOUND!!!! Thanks david <3

---
+++ src/arduino/pitch_roll_framework/pitch_roll_framework/pitch_roll_framework.ino
@@ -0,0 +1,121 @@
+#include <Arduino_LSM9DS1.h>
+#include <ros.h>
+#include <sensor_msgs/Imu.h>
+
+// Debug settings for serial printing.
+#define DEBUG 1
+#if DEBUG == 1
+#define debug(x) Serial.print(x)
+#define debugln(x) Serial.println(x)
+#else
+#define debug(x)
+#define debugln(x)
+#endif
+
+ros::
+
+geometry_msgs::Vector3 angular_velocity;
+ros::Publisher imuPub("imu_pub",&angular_velocity);
+
+// Timing variables for Loop
+uint32_t LoopTimer;
+int LastLoop;
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//1d Kalman filter setup
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+float RateRoll, RatePitch, RateYaw = 1; //If set to null, divide by zero in first iteration -> NaN output
+float RateCalibrationRoll, RateCalibrationPitch, RateCalibrationYaw;
+int RateCalibrationNumber;
+float AccX, AccY, AccZ = 1; //If set to null, divide by zero in first iteration -> NaN output
+float AccX_offset, Accy_offset, AccZ_offset;
+float AngleRoll, AnglePitch;
+uint32_t last_power_down;
+float KalmanAngleRoll = 0, KalmanUncertaintyAngleRoll = 2 * 2;
+float KalmanAnglePitch = 0, KalmanUncertaintyAnglePitch = 2 * 2;
+float Kalman1DOutput[] = { 0, 0 };
+bool startup = true;
+
+bool testStart = true;
+bool testEnd = true;
+int start;
+int end;
+
+void setup() {
+  LoopTimer = 0;
+
+  // Initialize Serial
+  Serial.begin(9600);
+  while (!Serial);
+  debugln("Begun");
+
+  // Initialize IMU
+  if (!IMU.begin()) {
+    debugln("Failed to initialize IMU!");
+    while (1);
+  }
+}
+
+void loop() {
+  LastLoop = micros();
+  // put your main code here, to run repeatedly:
+  // debugln() -- Use this instead of Print-- See lines 2-9
+
+  gyro_signals();
+  calculate_orientation();
+
+  debug(KalmanAngleRoll);
+  debug(", ");
+  debugln(KalmanAnglePitch);
+  
+  while (micros() - LoopTimer < 4000);
+  LoopTimer = micros();
+}
+
+void calculate_orientation() {
+  RateRoll -= RateCalibrationRoll;
+  RatePitch -= RateCalibrationPitch;
+  RateYaw -= RateCalibrationYaw;
+  kalman_1d(KalmanAngleRoll, KalmanUncertaintyAngleRoll, RateRoll, AngleRoll);
+  KalmanAngleRoll = Kalman1DOutput[0];
+  KalmanUncertaintyAngleRoll = Kalman1DOutput[1];
+  kalman_1d(KalmanAnglePitch, KalmanUncertaintyAnglePitch, RatePitch, AnglePitch);
+  KalmanAnglePitch = Kalman1DOutput[0];
+  KalmanUncertaintyAnglePitch = Kalman1DOutput[1];
+}
+
+//-------------------------------------------------------------------------------------------------------------------------------------
+//1D Kalman filter
+//inputs: KalmanState, KalmanUncertainty, KalmanInput, KalmanMeasurement => Kalman filter inputs
+//outputs: Kalman1DOutput => Gives Kalman filter outputs
+//-------------------------------------------------------------------------------------------------------------------------------------
+void kalman_1d(float KalmanState, float KalmanUncertainty, float KalmanInput, float KalmanMeasurement) {
+  //Don't forget that 0.004 is the time separation (4 ms)
+  KalmanState = KalmanState + 0.004 * KalmanInput;
+  KalmanUncertainty = KalmanUncertainty + 0.004 * 0.004 * 4 * 4;
+  float KalmanGain = KalmanUncertainty * 1 / (1 * KalmanUncertainty + 2); //Edited from original document as Kalman Gain factor was too high for sensitivty of BLE 33 sense's IMU
+  KalmanState = KalmanState + KalmanGain * (KalmanMeasurement - KalmanState);
+  KalmanUncertainty = (1 - KalmanGain) * KalmanUncertainty;
+  Kalman1DOutput[0] = KalmanState;
+  Kalman1DOutput[1] = KalmanUncertainty;
+}
+//-------------------------------------------------------------------------------------------------------------------------------------
+//Get Gyroscope signals
+//inputs: AccX, AccY => For integrating
+//outputs: AngleRoll, AnglePitch => Integrated
+//-------------------------------------------------------------------------------------------------------------------------------------
+void gyro_signals(void) {
+  //if (startup) {delay(10); startup = false;} //Fixed by setting initial RateR,P,Y and AccX,Y,Z to 1 instead of 0. Ensure functionality with EMo
+  
+  if (IMU.gyroscopeAvailable()) {
+    IMU.readGyroscope(RateRoll, RatePitch, RateYaw); //Provides angular velocity in degrees/sec
+    //RateRoll -> gyro x, RatePitch -> gyro y, RateYaw -> gyro z
+  }
+
+  if (IMU.accelerationAvailable()) {
+    IMU.readAcceleration(AccX, AccY, AccZ); //Provides angular velocity in degrees/sec
+  }
+
+  AngleRoll = atan(AccY / sqrt(AccX * AccX + AccZ * AccZ)) * 1 / (3.142 / 180);
+  AnglePitch = -atan(AccX / sqrt(AccY * AccY + AccZ * AccZ)) * 1 / (3.142 / 180);
+}
--- src/arduino/ultrasonic_sensor/ultrasonic_sensor.ino
+++ src/arduino/ultrasonic_sensor/ultrasonic_sensor.ino
@@ -1,77 +1,87 @@
-// ---------------------------------------------------------------- //
-// Arduino Ultrasoninc Sensor HC-SR04
-// Re-writed by Arbi Abdul Jabbaar
-// Using Arduino IDE 1.8.7
-// Using HC-SR04 Module
-// Tested on 17 September 2019
-// ---------------------------------------------------------------- //
+#include <ros.h>
+#include <std_msgs/Float32MultiArray.h>
+#include <NewPing.h>
 
-#define echoPin 3 // attach pin D2 Arduino to pin Echo of HC-SR04
-#define trigPin 2 //attach pin D3 Arduino to pin Trig of HC-SR04
 
-// defines variables
-long duration; // variable for the duration of sound wave travel
-long curDuration;
-//long distance; // variable for the distance measurement
-String distance;
-String longDistance;
+// trigger and echo pins for each sensor
+#define TRIG1 2
+#define ECHO1 3
+
+#define TRIG2 4
+#define ECHO2 5
+
+#define TRIG3 6
+#define ECHO3 7
+
+#define TRIG4 8
+#define ECHO4 9
+
+#define MAX_DISTANCE 100 // maximum distance for sensors in cm
+#define NUM_SONAR 4 // number of ultrasonic sensors
+
+NewPing sonar[NUM_SONAR] = { // array of ultrasonic pings
+  NewPing(TRIG1, ECHO1, MAX_DISTANCE),
+  NewPing(TRIG2, ECHO2, MAX_DISTANCE),
+  NewPing(TRIG3, ECHO3, MAX_DISTANCE),
+  NewPing(TRIG4, ECHO4, MAX_DISTANCE)
+};
+
+double curDuration[NUM_SONAR]; // array for pings in microseconds
+double prevDistance[NUM_SONAR]; // arrays for pings
+double curDistance[NUM_SONAR];// in cm
+
+// arrays to store distance output as string
+String curDistanceCM[NUM_SONAR];
+String curDistanceOutput[NUM_SONAR];
+
+// ros node, message, and publisher setup
+ros::NodeHandle nh;
+float ultraArray[4]; // initialize array to assign to msg
+std_msgs::Float32MultiArray msg;
+ros::Publisher pub("ultrasonic_pub", &msg);
 
 void setup() {
-  pinMode(trigPin, OUTPUT); // Sets the trigPin as an OUTPUT
-  pinMode(echoPin, INPUT); // Sets the echoPin as an INPUT
-  Serial.begin(9600); // // Serial Communication is starting with 9600 of baudrate speed
-  Serial.println("Ultrasonic Sensor HC-SR04 Test"); // print some text in Serial Monitor
-  Serial.println("with Arduino UNO R3");
+  // node and publisher setup
+  nh.initNode();
+  nh.advertise(pub);
+  msg.data_length = 4; // initialize length of msg array
 }
+
 void loop() {
-  longDistance = "";
-  // Clears the trigPin condition
-  for (int i = 0; i < 4; i++) {
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  // Sets the trigPin HIGH (ACTIVE) for 10 microseconds
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-  // Reads the echoPin, returns the sound wave travel time in microseconds
-  curDuration = pulseIn(echoPin, HIGH);
-  // Calculating the distance
-  curDuration = ( curDuration * 0.034 ) / 2; // Speed of sound wave divided by 2 (go and back)
-  curDuration = expFilter(0.3, duration, curDuration);
-  // Displays the distance on the Serial Monitor
-  //distance = (String)duration;
-  //distance = stringPadding(4, (String)curDuration);
-  if (curDuration > 600) {
-    curDuration = duration;
+  updateSonar(); // update the distance array
+ 
+  pub.publish(&msg); // publish the array of distance values
+  nh.spinOnce();
+}
+
+// takes a new reading from each sensor and updates the
+// distance array
+void updateSonar() {
+  for (int i = 0; i < NUM_SONAR; i++) {
+    curDuration[i] = sonar[i].ping(); // ping distance in microseconds
+    // sonar sensors return 0 if no obstacle is detected
+
+    curDistance[i] = (curDuration[i] * 0.034) / 2; // convert microseconds to cm
+
+    // if no object is detected set current distance to previous 
+    if (curDistance[i] == 0){
+      curDistanceOutput[i] = prevDistance[i];
+    }
+    else{
+      curDistance[i] = expFilter(0.1, prevDistance[i], curDistance[i]); // filter distance values
+      curDistanceCM[i] = String(curDistance[i]); // convert distance in cm to string
+      curDistanceOutput[i] = curDistanceCM[i].substring(0,curDistanceCM[i].length()-1); // concatanate cm with decimal mm
+    }  
+
+    ultraArray[i] = curDistanceOutput[i].toFloat(); // convert output to float and assign to
+    prevDistance[i] = curDistance[i]; // set previous distance to current 
+
   }
-  distance = (String)curDuration;
-  longDistance += stringPadding(4, distance);
-  if (curDuration < 31) {
-    Serial.println("B3");
-  }
-  else if (curDuration < 62) {
-    Serial.println("B2");
-  }
-  duration = curDuration;
   
-  delay(50);
-  }
-  Serial.println(longDistance);
+  msg.data = ultraArray; // update msg with curent array of values
 }
 
-
-String stringPadding(int requiredDigits, String value) {
-  int diff = requiredDigits - value.length();
-  while (diff > 0) {
-    value = "0" + value;
-    diff = diff - 1;
-  }
-  return value;
-}
-
-// test for different sensors
-// use this within each method for the sensors
+// method for filtering distance values
 double expFilter(double alpha, double prevReading, double curReading){
-  // Serial.println(alpha);
   return (alpha * curReading) + ((1 - alpha) * prevReading);
 }
