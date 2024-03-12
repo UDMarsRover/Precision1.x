d055f31bb647ff81a2a39822835fbd95e31ea79f
Ben Russell <ben@benjamindrussell.com>
2024-03-12 19:38:53 -0400

    bug fix

--- src/rover_pkg/MRCamera/gstreamer.py
+++ src/rover_pkg/MRCamera/gstreamer.py
@@ -20,12 +20,13 @@
 import gi
 gi.require_version('Gst','1.0')
 gi.require_version('GstVideo', '1.0')
+gi.require_version('GstRtspServer', '1.0')
 from gi.repository import GObject, Gst, GstVideo, GstRtspServer
 
 Gst.init(None)
 
 
-mainloop = GObject.MainLoop()
+mainloop = GLib.MainLoop()
 
 server = GstRtspServer.RTSPServer()
 
@@ -35,6 +36,8 @@ factory = GstRtspServer.RTSPMediaFactory()
 factory.set_launch('( v4l2src ! videoconvert ! x264enc speed-preset=ultrafast tune=zerolatency ! rtph264pay name=pay0 pt=96 )')
 
 mounts.add_factory("/test", factory)
+
+print("streaming...")
 
 server.attach(None)
 
