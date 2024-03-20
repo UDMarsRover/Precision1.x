e0d16161e9bd76c3cb433692a99e638889dc52f1
gregmolskow <greg.molskow@gmail.com>
2024-03-19 21:37:07 -0400

    updated formating to fix my linting issues

--- src/rover_pkg/controller_teleop.py
+++ src/rover_pkg/controller_teleop.py
@@ -5,8 +5,6 @@ import time
 import numpy as np
 from inputs import get_gamepad
 
-
-
 class udmrtMotorController:
     def __init__(self):
         rospy.init_node("Controller_teleop", anonymous=True)
@@ -23,23 +21,23 @@ class udmrtMotorController:
         self.current_start_state = 0
 
         self.buttonBuffer = {
-        "left_joy_y"  :0,
-        "left_joy_x"  :0,
-        "right_joy_y" :0,
-        "right_joy_x" :0,
-        "dpad_x":"",
-        "dpad_y":"",
-        "back":"",
-        "start":"0",
-        "a":"",
-        "b":"",
-        "x":"",
-        "y":"",
-        "rb":"",
-        "lb":"",
-        "rt":"",
-        "lt":""
-    }
+            "left_joy_y": 0,
+            "left_joy_x": 0,
+            "right_joy_y": 0,
+            "right_joy_x": 0,
+            "dpad_x": "",
+            "dpad_y": "",
+            "back": "",
+            "start": "0",
+            "a": "",
+            "b": "",
+            "x": "",
+            "y": "",
+            "rb": "",
+            "lb": "",
+            "rt": "",
+            "lt": ""
+        }
 
     def spin(self):
         self.__getInput__()
@@ -53,11 +51,10 @@ class udmrtMotorController:
         linVelY_temp = linVelY_temp if np.abs(linVelY_temp) > 0.1 else 0
         angVelZ_temp = angVelZ_temp if np.abs(angVelZ_temp) > 0.1 else 0
 
-        valueCheck = bool((self.linVelY !=  linVelY_temp) or 
-                      (angVelZ_temp != self.angVelZ))
+        valueCheck = bool((self.linVelY != linVelY_temp) or
+                     (angVelZ_temp != self.angVelZ))
         self.linVelY = linVelY_temp
         self.angVelZ = angVelZ_temp
-
 
         print(valueCheck)
 
@@ -76,28 +73,45 @@ class udmrtMotorController:
         :return: An updated version of the buttonBuffer variable
         :rtype: List
         """
-    
+
         event = get_gamepad()[0]
-        if      event.code == "BTN_THUMB"   : self.buttonBuffer["a"] = event.state
-        elif    event.code == "BTN_THUMB2"  : self.buttonBuffer["b"] = event.state
-        elif    event.code == "BTN_TRIGGER" : self.buttonBuffer["x"] = event.state
-        elif    event.code == "BTN_TOP"     : self.buttonBuffer["y"] = event.state
-        elif    event.code == "ABS_Y"       : self.buttonBuffer["left_joy_y"] = f"{round((int(event.state) - 128)/-128,1)}"
-        elif    event.code == "ABS_X"       : self.buttonBuffer["left_joy_x"] = f"{round((int(event.state) - 128)/128,1)}"
-        elif    event.code == "ABS_RZ"      : self.buttonBuffer["right_joy_y"] = f"{round((int(event.state) - 128)/-128,1)}"
-        elif    event.code == "ABS_Z"       : self.buttonBuffer["right_joy_x"] = f"{round((int(event.state) - 128)/128,1)}"
-        elif    event.code == "ABS_HAT0X"   : self.buttonBuffer["dpad_x"] = event.state
-        elif    event.code == "ABS_HAT0Y"   : self.buttonBuffer["dpad_y"] = str(int(event.state) * -1)
-        elif    event.code == "BTN_BASE4"   : self.buttonBuffer["start"] = event.state
-        elif    event.code == "BTN_BASE3"   : self.buttonBuffer["back"] = event.state
-        elif    event.code == "BTN_PINKIE"  : self.buttonBuffer["rb"] = event.state
-        elif    event.code == "BTN_BASE2"   : self.buttonBuffer["rt"] = event.state
-        elif    event.code == "BTN_TOP2"    : self.buttonBuffer["lb"] = event.state
-        elif    event.code == "BTN_BASE"    : self.buttonBuffer["lt"] = event.state
-        
+        if event.code == "BTN_THUMB":
+            self.buttonBuffer["a"] = event.state
+        elif event.code == "BTN_THUMB2":
+            self.buttonBuffer["b"] = event.state
+        elif event.code == "BTN_TRIGGER":
+            self.buttonBuffer["x"] = event.state
+        elif event.code == "BTN_TOP":
+            self.buttonBuffer["y"] = event.state
+        elif event.code == "ABS_Y":
+            self.buttonBuffer["left_joy_y"] = f"{round((int(event.state) - 128)/-128,1)}"
+        elif event.code == "ABS_X":
+            self.buttonBuffer["left_joy_x"] = f"{round((int(event.state) - 128)/128,1)}"
+        elif event.code == "ABS_RZ":
+            self.buttonBuffer["right_joy_y"] = f"{round((int(event.state) - 128)/-128,1)}"
+        elif event.code == "ABS_Z":
+            self.buttonBuffer["right_joy_x"] = f"{round((int(event.state) - 128)/128,1)}"
+        elif event.code == "ABS_HAT0X":
+            self.buttonBuffer["dpad_x"] = event.state
+        elif event.code == "ABS_HAT0Y":
+            self.buttonBuffer["dpad_y"] = str(int(event.state) * -1)
+        elif event.code == "BTN_BASE4":
+            self.buttonBuffer["start"] = event.state
+        elif event.code == "BTN_BASE3": 
+            self.buttonBuffer["back"] = event.state
+        elif event.code == "BTN_PINKIE":
+            self.buttonBuffer["rb"] = event.state
+        elif event.code == "BTN_BASE2":
+            self.buttonBuffer["rt"] = event.state
+        elif event.code == "BTN_TOP2":
+            self.buttonBuffer["lb"] = event.state
+        elif event.code == "BTN_BASE":
+            self.buttonBuffer["lt"] = event.state
+
         return self.buttonBuffer
+
 
 controller = udmrtMotorController()
 
-while not rospy.is_shutdown() :
+while not rospy.is_shutdown():
     controller.spin()
