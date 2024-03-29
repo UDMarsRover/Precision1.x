06fd3db4e302a4c85760e58ac8c552d7e3c684dd
gregmolskow <greg.molskow@gmail.com>
2024-03-28 21:59:31 -0400

    gpio shutdown test

--- src/rover_pkg/main.py
+++ src/rover_pkg/main.py
@@ -30,11 +30,17 @@ def led_control(r: int, g: int, b: int):
 def shutdown(check: bool = True):
     if check:
         if not gpio.input(shutdownPin):
-            gpio.cleanup(indicatorLED,shutdownPin)
+            gpio.cleanup(indicatorLED["red"])
+            gpio.cleanup(indicatorLED["blue"])
+            gpio.cleanup(indicatorLED["green"])
+            gpio.cleanup(shutdownPin)
             os.system("systemctl poweroff")
             led_control(1, 1, 0)
     else:
-        gpio.cleanup(indicatorLED,shutdownPin)
+        gpio.cleanup(indicatorLED["red"])
+        gpio.cleanup(indicatorLED["blue"])
+        gpio.cleanup(indicatorLED["green"])
+        gpio.cleanup(shutdownPin)
         os.system("systemctl poweroff")
         led_control(1, 1, 0)
 
