c8a869437781dc71a026e106696a59aa5dceb0e8
gregmolskow <greg.molskow@gmail.com>
2024-03-29 00:06:43 -0400

    shutdown test

--- src/rover_pkg/main.py
+++ src/rover_pkg/main.py
@@ -8,11 +8,11 @@ from std_msgs.msg import String
 import RPi.GPIO as gpio
 import dbus
 sys_bus = dbus.SystemBus()
-hal_srvc = sys_bus.get_object('org.freedesktop.Hal',
-                              '/org/freedesktop/Hal/devices/computer')
-pwr_mgmt =  dbus.Interface(hal_srvc,
-                'org.freedesktop.Hal.Device.SystemPowerManagement')
-shutdown_method = pwr_mgmt.get_dbus_method("Shutdown")
+#hal_srvc = sys_bus.get_object('org.freedesktop.Hal',
+#                              '/org/freedesktop/Hal/devices/computer')
+#pwr_mgmt =  dbus.Interface(hal_srvc,
+#                'org.freedesktop.Hal.Device.SystemPowerManagement')
+#shutdown_method = pwr_mgmt.get_dbus_method("Shutdown")
 
 
 # Create a rover instance
