1bf174fcc9d9ec929f14a0705e1bd00d599288f4
gregmolskow <greg.molskow@gmail.com>
2024-03-31 18:31:35 -0400

    code restart test

--- src/rover_pkg/main.py
+++ src/rover_pkg/main.py
@@ -75,7 +75,7 @@ if __name__ == "__main__":
     # While roscore is running
     while not rospy.is_shutdown():
         led_control(0, 1, 0)
-        shutdown(wifiConnected())
+        shutdown()
 
 
     
