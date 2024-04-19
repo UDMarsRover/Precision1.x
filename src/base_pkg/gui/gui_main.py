4dc9b2f3d949406ec2ef4fe73448b10b17868e0a
plachkik <114035182+plachkik@users.noreply.github.com>
2024-04-18 22:18:06 -0400

    Update gui_main.py, PAULS FAULT!!!

--- src/base_pkg/gui/gui_main.py
+++ src/base_pkg/gui/gui_main.py
@@ -1,52 +1,3 @@
-<<<<<<< HEAD
-import sys
-
-from PyQt5.QtCore import QSize, Qt
-from PyQt5.QtWidgets import (
-    QApplication, 
-    QMainWindow, 
-    QWidget, 
-    QHBoxLayout, 
-    QVBoxLayout, 
-    QLabel, 
-    QStackedLayout
- )
-
-from PyQt5.QtGui import QPalette, QColor
-
-class P(QMainWindow):
-
-    def __init__(self):
-        super().__init__()
-
-        self.setFixedSize(QSize(600, 200))
-
-        layout = QHBoxLayout()
-
-        emoLabel = QLabel("Emo Error")
-        emoLabel.setStyleSheet("background-color: red; color: white;")
-        emoLabel.setAlignment(Qt.AlignCenter)
-        layout.addWidget(emoLabel)
-
-        driveLabel = QLabel("Drive Error")
-        driveLabel.setStyleSheet("background-color: red; color: white;")
-        driveLabel.setAlignment(Qt.AlignCenter)
-        layout.addWidget(driveLabel)
-
-        armLabel = QLabel("Arm Error")
-        armLabel.setStyleSheet("background-color: red; color: white")
-        armLabel.setAlignment(Qt.AlignCenter)
-        layout.addWidget(armLabel)
-
-        camLabel = QLabel("Camera Error")
-        camLabel.setStyleSheet("background-color: red; color: white")
-        camLabel.setAlignment(Qt.AlignCenter)
-        layout.addWidget(camLabel)
-        
-        widget = QWidget()
-        widget.setLayout(layout)
-        self.setCentralWidget(widget)
-=======
 import gui_per_1 as per1
 import gui_per_2 as per2
 
@@ -114,18 +65,9 @@ class gui_pers(QVBoxLayout):
 def show(self):
     return self
 
-
->>>>>>> cd9b0e05b26f0b2f0eb55c811c6ab04a0d93c313
-
 if __name__ == "__main__":
 
     app = QApplication(sys.argv)
-<<<<<<< HEAD
-    window = P()
-    window.show()
-
-    app.exec()
-=======
 
     window = MainWindow()
     window.show()
@@ -135,4 +77,3 @@ if __name__ == "__main__":
 
     app.exec()
 
->>>>>>> cd9b0e05b26f0b2f0eb55c811c6ab04a0d93c313
