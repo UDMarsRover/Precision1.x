cf4cc3228a90964386f63a40c317a03b01cae6ec
Ryan Kuederle <ryankuederle@icloud.com>
2024-04-23 20:26:50 -0400

    fixed import

--- src/rover_pkg/camera_servo.py
+++ src/rover_pkg/camera_servo.py
@@ -3,14 +3,14 @@
 import rospy
 from std_msgs.msg import Int8
 import time
-import RPi.GPIO
+import RPi.GPIO as gpio
 
 class CameraServoNode:
     def __init__(self):
         self.servo_pin = 17
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(self.servo_pin, GPIO.OUT)
-        self.p = GPIO.PWM(self.servo_pin, 50)
+        gpio.setmode(gpio.BCM)
+        gpio.setup(self.servo_pin, gpio.OUT)
+        self.p = gpio.PWM(self.servo_pin, 50)
         self.p.start(2.5) # duty cycle 2.5%
         rospy.init_node("camera_servo_node", anonymous=True)
         self.sub = rospy.Subscriber("/base/gui/out/camera_servo", Int8, self.callback)
