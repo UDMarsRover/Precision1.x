5836dd4c7fdbcf284b6f15d54ff38b10f43ca488
gregmolskow <greg.molskow@gmail.com>
2024-04-28 18:20:13 -0400

    stop when disconnected form wifi

--- src/rover_pkg/main.py
+++ src/rover_pkg/main.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 from std_msgs.msg import String
+from geometry_msg.msg import Twist
 from rover import Rover
 import time
+import rospy
+
+drive = rospy.Publisher("DriveVelocity", Twist,queue_size=1)
 
 # The main Loop
 if __name__ == "__main__":
@@ -11,6 +15,12 @@ if __name__ == "__main__":
     killCount = 0
     while not p1.kill:
         p1.spin()
+    
+    if not p1.wifiConnected:
+        stop = Twist()
+        stop.linear.y = 0
+        stop.angular.z = 0
+        drive.publish(stop)
         
     p1.log("Rover Killed")
     p1.led_control(0,1,1)
