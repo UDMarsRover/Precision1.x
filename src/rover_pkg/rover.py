cd72718be21d2332c664f1e81deb54588004d02c
gregmolskow <greg.molskow@gmail.com>
2024-04-28 18:45:17 -0400

    wifi detect

--- src/rover_pkg/rover.py
+++ src/rover_pkg/rover.py
@@ -123,7 +123,7 @@ class Rover:
     def wifiCheck(self, ip:str = "192.168.8.1"):
         
         self.wifiConnected = not (os.system(f"ping -c 1 "+ip) == 0)
-        print(os.system(f"ping -c 1 "+ip))
+        print("****: ", os.system(f"ping -c 1 "+ip))
         return self.wifiConnected
 
 
