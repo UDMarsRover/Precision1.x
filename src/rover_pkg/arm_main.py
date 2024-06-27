cd0fd702cab09da3096ad58106da769277940875
gregmolskow <greg.molskow@gmail.com>
2024-06-26 22:13:27 -0400

    structure test

--- src/rover_pkg/arm_main.py
+++ src/rover_pkg/arm_main.py
@@ -7,7 +7,7 @@ from std_msgs.msg import Float32MultiArray
 from std_msgs.msg import Bool
 from trajectory_msgs.msg import JointTrajectoryPoint
 
-from src.UDMRT_datatypes import Arm_Position
+from UDMRT_datatypes import Arm_Position
 
 
 class Arm:
