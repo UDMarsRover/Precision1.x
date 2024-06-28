5df9ef7f95339e350ad327673b7372908ebdeef7
gregmolskow <greg.molskow@gmail.com>
2024-06-28 19:21:44 -0400

    update

--- src/rover_pkg/arm_main.py
+++ src/rover_pkg/arm_main.py
@@ -62,8 +62,8 @@ class Arm:
             #self.robot.arm.move_pose(self.currentPosition.getState(),callback=self.temp_callback)
             print("Published ", jog_values)
     
-            self.robot.arm.jog_pose(jog_values,callback=self.temp_callback)
-            self.robot.arm.move_joints([jog_values[0],0,0,0,0,0],callback=self.temp_callback)
+            #self.robot.arm.jog_pose(jog_values,callback=self.temp_callback)
+            self.robot.arm.jog_joints([jog_values[0],0,0,0,0,0],callback=self.temp_callback)
         #print("Ran")
 
     def callback_grip_command(self, msg):
