97c982e58b838a4e9e6a8720932e9a742ff454a5
gregmolskow <greg.molskow@gmail.com>
2024-06-28 20:39:14 -0400

    added jog callback

--- src/rover_pkg/arm_main.py
+++ src/rover_pkg/arm_main.py
@@ -63,7 +63,7 @@ class Arm:
             print("Published ", [0,0,0,jog_values[0],0,0])
     
             #self.robot.arm.jog_pose(jog_values,callback=self.temp_callback)
-            self.robot.arm.jog_joints([0,0,0,jog_values[0],0,0])#,callback=self.temp_callback)
+            self.robot.arm.jog_joints([0,0,0,jog_values[0],0,0],callback=self.temp_callback)
         #print("Ran")
 
     def callback_grip_command(self, msg):
