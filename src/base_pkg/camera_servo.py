d88ec907bcddc6b1f36428939206f8ef585ef553
Ryan Kuederle <ryan@rkuederle.com>
2024-08-08 23:45:46 -0400

    Changed to 27

--- src/base_pkg/camera_servo.py
+++ src/base_pkg/camera_servo.py
@@ -4,7 +4,7 @@ import RPi.GPIO as gpio
 
 class ServoController:
     def __init__(self, pin):
-        self.servo_pin = 17
+        self.servo_pin = 27
         gpio.setmode(gpio.BCM)
         gpio.setup(self.servo_pin, gpio.OUT)
         self.p = gpio.PWM(self.servo_pin, 250)
