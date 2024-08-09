0f7f1056456bb6cecfdf11cddd082392a2496717
Ryan Kuederle <ryan@rkuederle.com>
2024-08-09 02:58:53 -0400

    idk

--- src/base_pkg/camera_servo.py
+++ src/base_pkg/camera_servo.py
@@ -13,7 +13,14 @@ class ServoController:
     def set_angle(self, angle):
         # duty_cycle = 34.6 + angle / 50.0
         # duty_cycle = angle
-        self.p.ChangeDutyCycle(angle)
+        if angle > 100:
+            self.p = gpio.PWM(self.servo_pin, 50)
+            self.p.ChangeDutyCycle(12.5)
+            time.sleep(0.1)
+            self.p.ChangeDutyCycle(0)
+        else:
+            self.p = gpio.PWM(self.servo_pin, 250)
+            self.p.ChangeDutyCycle(angle)
         time.sleep(0.5)
 
     def cleanup(self):
