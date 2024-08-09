6471804377e9e9402c029b2c976d9b737983971d
Ryan Kuederle <ryan@rkuederle.com>
2024-08-09 14:46:11 -0600

    back to 8080

--- src/rover_pkg/camera_server.py
+++ src/rover_pkg/camera_server.py
@@ -69,7 +69,7 @@ def index():
     return Response(webcam_feed(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 def main():
-    app.run(host='0.0.0.0', port=24)
+    app.run(host='0.0.0.0', port=8080)
 
 if __name__ == '__main__':
     main()
