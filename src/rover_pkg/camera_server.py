3875d4f5c004ae227edbf49d827e9ac6dfb46943
Ryan Kuederle <ryan@rkuederle.com>
2024-08-09 13:49:08 -0400

    Added Aruco Marker Detection to camera server

--- src/rover_pkg/camera_server.py
+++ src/rover_pkg/camera_server.py
@@ -1,20 +1,47 @@
 import cv2
 import numpy as np
 from flask import Flask, Response
+import time
 
 app = Flask(__name__)
 
+# Load the ArUco dictionary
+dictionary = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_100)
+parameters =  cv2.aruco.DetectorParameters()
+detector = cv2.aruco.ArucoDetector(dictionary, parameters)   
+
 def webcam_feed():
     cap = cv2.VideoCapture(0)
-
+    
     while True:
         ret, frame = cap.read()
 
         # Apply color correction for weird windows blue shift
         # Reduce the framerate by adding a delay
         # Uncomment the line below to reduce the framerate to 1 frame per second
-        cv2.waitKey(100)
         # frame = np.where(frame > 1, frame - 1, frame)
+        # Convert the frame to grayscale
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # Detect ArUco markers
+        corners, ids, rejected = detector.detectMarkers(gray)
+        if ids is not None:
+            for i in range(len(ids)):
+                # Draw outline of the marker on the current frame
+                # Get the corners of the marker
+                marker_corners = corners[i][0]
+                marker_corners = marker_corners.astype(int)
+                # Draw the outline of the marker
+                cv2.polylines(frame, [marker_corners], True, (0, 255, 0), 2)
+                # Put the text of the ID in the middle of the tag in the frame
+                # Calculate the center of the marker
+                center_x = int((marker_corners[0][0] + marker_corners[2][0]) / 2)
+                center_y = int((marker_corners[0][1] + marker_corners[2][1]) / 2)
+
+                # Draw the ID text on the frame
+                cv2.putText(frame, str(ids[i]), (center_x, center_y), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
 
         # Compress the frame for low latency
         _, compressed_frame = cv2.imencode('.jpg', frame, [cv2.IMWRITE_JPEG_QUALITY, 20])
@@ -22,6 +49,8 @@ def webcam_feed():
 
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
+        # cv2.waitKey(100)
+        time.sleep(0.03)
 
 @app.route('/')
 def index():
